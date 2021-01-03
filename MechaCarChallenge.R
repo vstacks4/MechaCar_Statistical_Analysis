d75752b733bac0100bf361d0fb2b87909df8c5fb
vstacks4 <69838791+vstacks4@users.noreply.github.com>
2021-01-03 00:43:18 -0600

    commiting updated files for challenge 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,3 +6,9 @@
 Suspension_coil_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
 Total_summary <- Suspension_coil_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create summary table
 lot_summary <- Suspension_coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+
+#Deliverable #3
+t.test(log10(Suspension_coil_table$PSI),mu=mean(log10(1500)))
+t.test(log10(suspension_coil_lot1$PSI), mu=mean(log10(1500)))
+t.test(log10(suspension_coil_lot2$PSI), mu=mean(log10(1500)))
+t.test(log10(suspension_coil_lot3$PSI), mu=mean(log10(1500)))
